15905e014c9a8fbd41b6905ae40f20c9803c07b1
Huang, Yexin <yexin_huang@intuit.com>
2016-11-17 14:12:32 -0600

    init project

---
+++ src/main/java/com/yh/graphql/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.yh.graphql;
+
+import graphql.GraphQL;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static graphql.Scalars.GraphQLString;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLObjectType.newObject;
+
+public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+
+        GraphQLObjectType queryType = newObject()
+                .name("helloWorldQuery")
+                .field(newFieldDefinition()
+                        .type(GraphQLString)
+                        .name("hello")
+                        .staticValue("world"))
+                .build();
+
+        GraphQLSchema schema = GraphQLSchema.newSchema()
+                .query(queryType)
+                .build();
+        Map<String, Object> result = (Map<String, Object>) new GraphQL(schema).execute("{hello}").getData();
+
+        System.out.println(result);
+        logger.info("result={}", result);
+    }
+}
